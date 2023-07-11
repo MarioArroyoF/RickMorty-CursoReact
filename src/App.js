f397dafd632f0a772d53e9c0ce2d1020e61d9eec
MarioArroyoF <18170275@itculiacan.edu.mx>
2023-07-10 18:40:10 -0700

    h
    h

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import imgRickMorty from './img/rick-morty.png';
+import './App.css';
+import { useState } from 'react';
+import Characters from './components/Characters';
+
+function App() {
+
+  const[characters,setcharacters]=useState(null);
+
+  const reqApi= async() =>{
+    const api = await fetch('https://rickandmortyapi.com/api/character');
+    const characterApi = await api.json();
+    setcharacters(characterApi.results);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1 className='title'>Rick & Morty</h1>
+       {characters ?(
+        <Characters characters={characters} setcharacters={setcharacters}/>
+       ):
+       <>
+       <img src={imgRickMorty} alt="Rick & Morty" className='img-home'></img>
+       <button onClick={reqApi} className='btn-search'>Buscar Personajes</button>
+       </>
+       }
+       
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
